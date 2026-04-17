b98e177600c4640e033e7472a2f8577cde0e8e42
VaunnInari <charlesborres44@gmail.com>
2026-04-17 20:48:46 +0800

    Task D2 - Complete

--- app/src/main/java/com/example/exercise4/Scratch.kt
+++ app/src/main/java/com/example/exercise4/Scratch.kt
@@ -1,12 +1,7 @@
 package com.example.exercise4
 
 fun main() {
-    var nickname: String? = null
-
-    println(nickname?.length)
-
-    nickname = "Charlie"
-
-    println(nickname?.length)
+    for (i in 1..5) {
+        println(i)
+    }
 }
-
