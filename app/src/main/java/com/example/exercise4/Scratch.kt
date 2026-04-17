5faa52d1af1516fb87af033bad99b1e35d556aaf
VaunnInari <charlesborres44@gmail.com>
2026-04-17 20:36:24 +0800

    Task A2 - Complete

---
+++ app/src/main/java/com/example/exercise4/Scratch.kt
@@ -0,0 +1,10 @@
+package com.example.exercise4
+
+fun main() {
+    val name = "Charles"
+    var age = 21
+
+    age += 1
+
+    println("My name is $name and I will be $age years old next year.")
+}
