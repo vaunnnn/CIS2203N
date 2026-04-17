b2abc426d6d25e2b19c55f0f16d9ac4a62da2ace
VaunnInari <charlesborres44@gmail.com>
2026-04-17 20:33:51 +0800

    Task A1 - Complete

---
+++ app/src/main/java/com/example/exercise4/CalculatorActivity.kt
@@ -0,0 +1,22 @@
+package com.example.exercise4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CalculatorActivity : AppCompatActivity() {
+
+    var currentInput = ""
+    var firstOperand = 0.0
+    var currentOperator = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
